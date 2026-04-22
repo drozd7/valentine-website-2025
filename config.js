32a3757e522fc628ee050dbb497a54f51d569c44
drozd7 <drozdovm748@gmail.com>
2026-04-22 17:10:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://wekibi.com/438", // Music streaming URL
+        musicUrl: "https://www.youtube.com/watch?v=eerVwauvAZU", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
